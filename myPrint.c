239462ca89c49db14592f63a290acca5199a1201
faraji-fuji <farajifuji@gmail.com>
2022-04-19 05:40:27 +0000

    project initialization

---
+++ myPrint.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _printf - Produces output according to a format.
+ *
+ * @format: format specification.
+ *
+ * Return: the number of characters printed (excluding the null byte used to
+ * end output to strings)
+ */
+
+int _printf(const char *format, ...)
+{
+
+}
